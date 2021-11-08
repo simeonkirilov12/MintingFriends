3dc5b8e3cfddcf93bde4f680530972b9a77d127b
Fat Ape Rocket Crew <simeonkirilov88@gmail.com>
2021-11-08 04:49:36 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -365,29 +365,6 @@ const App = () => {
               <h2>Rarity</h2>
               <StyledWrapper>
               
-              <table class="blueTable">
-                <thead>
-                <tr>
-                <th>head1</th>
-                <th>head2</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                <td>cell1_1</td><td>cell2_1</td></tr>
-                <tr>
-                <td>cell1_2</td><td>cell2_2</td></tr>
-                <tr>
-                <td>cell1_3</td><td>cell2_3</td></tr>
-                <tr>
-                <td>cell1_4</td><td>cell2_4</td></tr>
-                <tr>
-                <td>cell1_5</td><td>cell2_5</td></tr>
-                </tbody>
-                </tr>
-                </table>
-
-          
               
               <ul>
                 <li>🐔 Heads 10</li>
